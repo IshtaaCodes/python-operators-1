d285acb7c4f23ab4cdc60e9378add9ae84c9fa29
ishtaacodes <itsmeishtaa@gmail.com>
2025-12-19 18:39:12 +0530

    first commit

---
+++ activity2.py
@@ -0,0 +1,8 @@
+s1=float(input("enter marks for subject 1"))
+s2=float(input("enter marks for subject 2"))
+s3=float(input("enter marks for subject 3"))
+s4=float(input("enter marks for subject 4"))
+s5=float(input("enter marks for subject 5"))
+total=s1+s2+s3+s4+s5
+perc=(total/500)*100
+print("the percentage is",perc)
